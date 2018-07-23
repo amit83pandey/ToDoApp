24b87b89a7fedf3cdfc89c192fcb784c54e27e08
Amit Pandey <amit.pandey@agreeya.net>
2018-07-23 15:22:08 +0530

    test cases added

---
+++ src/test/java/com/emirates/springboot/ToDoAppTest.java
@@ -0,0 +1,14 @@
+package com.emirates.springboot;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ToDoAppTest {
+	
+	@Test
+	public void mockTest() {
+		assertEquals(1,1);
+	}
+
+}
